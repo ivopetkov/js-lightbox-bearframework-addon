bd9869ef7f6221a7a3a29decb38a8ab03d0d73b4
Ivo Petkov <ivo@ivopetkov.com>
2016-09-16 21:33:41 +0300

    Scripts position update.

--- components/jsLightbox.php
+++ components/jsLightbox.php
@@ -8,10 +8,10 @@
 ?><html>
     <head>
         <link id="js-lightbox-bearframework-addon-style" rel="stylesheet" href="<?= $context->assets->getUrl('assets/swiper.min.css') ?>">
-    </head>
-    <body>
         <script id="js-lightbox-bearframework-addon-script-1" src="<?= $context->assets->getUrl('assets/swiper.min.js') ?>"></script>
         <script id="js-lightbox-bearframework-addon-script-2" src="<?= $context->assets->getUrl('assets/jsLightbox.min.js') ?>"></script>
+    </head>
+    <body>
         <?php
         if (!empty($component->onload)) {
             echo '<script>' . $component->onload . '</script>';
