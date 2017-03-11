def3dc66569e0995f849589c29417cd80601c996
Ivo Petkov <ivo@ivopetkov.com>
2017-03-11 07:19:11 +0200

    Assets optimizations.

--- components/jsLightbox.php
+++ components/jsLightbox.php
@@ -12,10 +12,10 @@ $app = App::get();
 $context = $app->context->get(__FILE__);
 ?><html>
     <head>
-        <link id="js-lightbox-bearframework-addon-style-1" rel="stylesheet" href="<?= $context->assets->getUrl('assets/jsLightbox.min.css') ?>">
-        <link id="js-lightbox-bearframework-addon-style-2" rel="stylesheet" href="<?= $context->assets->getUrl('assets/swiper.min.css') ?>">
-        <script id="js-lightbox-bearframework-addon-script-1" src="<?= $context->assets->getUrl('assets/swiper.min.js') ?>"></script>
-        <script id="js-lightbox-bearframework-addon-script-2" src="<?= $context->assets->getUrl('assets/jsLightbox.min.js') ?>"></script>
+        <link id="js-lightbox-bearframework-addon-style-1" rel="stylesheet" href="<?= $context->assets->getUrl('assets/jsLightbox.min.css', ['cacheMaxAge' => 999999, 'version' => 1]) ?>">
+        <link id="js-lightbox-bearframework-addon-style-2" rel="stylesheet" href="<?= $context->assets->getUrl('assets/swiper.min.css', ['cacheMaxAge' => 999999, 'version' => 1]) ?>">
+        <script id="js-lightbox-bearframework-addon-script-1" src="<?= $context->assets->getUrl('assets/swiper.min.js', ['cacheMaxAge' => 999999, 'version' => 1]) ?>"></script>
+        <script id="js-lightbox-bearframework-addon-script-2" src="<?= $context->assets->getUrl('assets/jsLightbox.min.js', ['cacheMaxAge' => 999999, 'version' => 1]) ?>"></script>
     </head>
     <body>
         <?php
