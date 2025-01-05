2981cca7097ad67bc27b88726f7a22fb00dca5bd
Ivo Petkov <ivo@ivopetkov.com>
2025-01-05 13:29:45 +0200

    Add PHP 8.4 support. Remove PHP 7.* support.

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ $app->localization
     });
 
 $app->clientPackages
-    ->add('lightbox', function (IvoPetkov\BearFrameworkAddons\ClientPackage $package) use ($context) {
+    ->add('lightbox', function (IvoPetkov\BearFrameworkAddons\ClientPackage $package) use ($context): void {
         $closeButtonIcon = '<svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff"><path d="M11.47 10l7.08-7.08c.4-.4.4-1.06 0-1.47-.4-.4-1.06-.4-1.47 0L10 8.53 2.92 1.45c-.4-.4-1.07-.4-1.47 0-.4.4-.4 1.06 0 1.47L8.53 10l-7.08 7.08c-.4.4-.4 1.07 0 1.47.2.2.47.3.74.3.23 0 .5-.1.7-.3l7.1-7.08 7.07 7.08c.2.2.47.3.73.3.3 0 .56-.1.76-.3.4-.4.4-1.06 0-1.47L11.46 10z"/></svg>';
 
         $code = '.ipjslghtbc{opacity:0;position:fixed;z-index:10010000;top:0;left:0;width:100%;height:100%;background-color:rgba(0,0,0,0.83);backdrop-filter:blur(4px);-webkit-transition:opacity 300ms;transition:opacity 300ms;}'
@@ -32,10 +32,6 @@ $app->clientPackages
             . '.ipjslghtbx{display:block;width:42px;height:42px;position:fixed;right:0;top:0;cursor:pointer;z-index:20000001;user-select:none;-moz-user-select:none;-khtml-user-select:none;-webkit-user-select:none;-o-user-select:none;background-image:url(data:image/svg+xml;base64,' . base64_encode($closeButtonIcon) . ');background-repeat:no-repeat;background-position:center;background-size:16px;}'
             . '.ipjslghtbcl{animation:ipjslghtbw 1.5s infinite linear;position:absolute;width:36px;height:36px;margin-top:-22px;margin-left:-22px;box-sizing:border-box;border-radius:50%;border:4px solid rgba(255,255,255,0.2);cursor:default;user-select:none;-moz-user-select:none;-khtml-user-select:none;-webkit-user-select:none;-o-user-select:none;opacity:0;-webkit-transition:opacity 300ms;transition:opacity 300ms;border-right:4px solid rgba(255,255,255,0.8);}'
             . '.ipjslghtbclv{opacity:1;}'
-            //                    . '@-moz-keyframes ipjslghtbw{from{-moz-transform:rotate(0deg);}to{-moz-transform:rotate(360deg);}}'
-            //                    . '@-webkit-keyframes ipjslghtbw{from{-webkit-transform:rotate(0deg);}to{-webkit-transform:rotate(360deg);}}'
-            //                    . '@-ms-keyframes ipjslghtbw{from{-ms-transform:rotate(0deg);}to{-ms-transform:rotate(360deg);}}'
-            //                    . '@-o-keyframes ipjslghtbw{from{transform:rotate(0deg);}to{transform:rotate(360deg);}}'
             . '@keyframes ipjslghtbw{from{transform:rotate(0deg);}to{transform:rotate(360deg);}}';
         $package->addCSSCode($code);
 
